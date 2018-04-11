d69e514def528fd359fac9279f1bb54f1ac71ea1
Matthew Anderson <matthew.anderson@mmediadesign.co.uk>
2018-04-11 17:00:37 +0100

    route model binding

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,9 +1,5 @@
 <?php
-/**
-* Page Controller: Entry point for generic pages
-* @author Matt Anderson
-* 
-*/
+
 namespace App\Http\Controllers;
 
 use Exception;
@@ -11,32 +7,14 @@ use Illuminate\Http\Request;
 use App\PageTemplate;
 use App\Page;
 
-/**
-* Page Controller: Class definition
-* @author Matt Anderson
-* 
-*/
 class PageController extends Controller
 {
     private $_template;
 
-    /**
-    *
-    * @author Matt Anderson
-    * 
-    */
     public function __construct(PageTemplate $_template) {
         $this->template = $_template;
     }
 
-    /**
-    * Enrty point for generic pages.
-    * Added means of controlling template that is rendered using DB field 
-    * in corresponding table.
-    *
-    * @author Matt Anderson
-    * 
-    */
     public function index(Request $request) {
         try {
             $strRequestPath = (string)$request->path();
@@ -55,5 +33,9 @@ class PageController extends Controller
                 404
             );
         }
+    }
+
+    public function routeModelBinding(Page $page) {
+        dd($page);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/routeModelBinding/{page}', 'PageController@routeModelBinding');
