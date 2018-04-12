23f1a3b60196e2e4044ad3b6b917301388a9430f
Matthew Anderson <matthew.anderson@mmediadesign.co.uk>
2018-04-12 20:43:11 +0100

    adding user Laravels out of the box auth, including catering for user setting field

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/routeModelBinding/{page}', 'PageController@routeModelBinding');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
